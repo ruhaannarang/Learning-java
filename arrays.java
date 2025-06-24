188fae309e5a85ae6d08263e6e10c90f49954398
Ruhaan Narang <techieruhaan@gmail.com>
2025-06-24 15:39:15 +0530

    array

---
+++ arrays.java
@@ -0,0 +1,24 @@
+public class arrays {
+    public static void main(String[]args){
+        //list of items of the same type
+
+        //Defining array
+        //1st method
+
+//        int[]marks=new int[3];
+//        marks[0]=99;
+//        marks[1]=100;
+//        marks[2]=97;
+//        System.out.println(marks[0]);
+//        System.out.println(marks[1]);
+//        System.out.println(marks[2]);
+//        for (int i=0;i<3;i++){
+//            System.out.println(marks[i]);
+//        }
+        //2nd method
+        int marks[]={44,55,66};
+        for (int i=0;i<3;i++) {
+            System.out.println(marks[i]);
+        }
+    }
+}
