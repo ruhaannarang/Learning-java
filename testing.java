d4d67782add729ee46dc7ad02f0aa764da762fd9
Ruhaan Narang <techieruhaan@gmail.com>
2025-07-01 15:44:20 +0530

    kuch strings ki chize

---
+++ testing.java
@@ -0,0 +1,6 @@
+public class testing {
+    public static void main(String[]args){
+        String a="rrr";
+        System.out.println((a.length())/2);
+    }
+}
---
+++ toString.java
@@ -0,0 +1,11 @@
+public class toString {
+    public static void main(String args[]) {
+        int number = 123;
+        String str = Integer.toString(number);
+        System.out.println(str);
+        System.out.println(str.length());
+
+
+    }
+
+}
