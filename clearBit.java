6de291429c68215d2630cb14ae176529599fe096
Ruhaan Narang <techieruhaan@gmail.com>
2025-07-11 12:35:07 +0530

    clear bit

---
+++ clearBit.java
@@ -0,0 +1,11 @@
+public class clearBit {
+    public static void main(String[]args){
+        int n=5;
+        int pos=2;
+        int bitmask=1<<pos;
+
+        int ans = ~bitmask;
+        int finalAns = ans & n;
+        System.out.println(finalAns);
+    }
+}
