1b9a4dae01bef50854ee1879174ef2e5c56b9369
Ruhaan Narang <techieruhaan@gmail.com>
2025-07-11 12:24:59 +0530

    get bit

---
+++ getbit.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class getbit {
+    public static void main(String[]args){
+        int n=8;
+        int pos=2;
+        int bitmask=1<<pos;
+
+        if ((bitmask & n) ==0){
+            System.out.println("bit is 0");
+        }
+        else{
+            System.out.println("Bit is 1");
+        }
+    }
+}
