e12f33450d81512b67924fbbe3c8a8d1dcda3630
Ruhaan Narang <techieruhaan@gmail.com>
2025-10-12 13:36:28 +0530

    recursion power question

--- calcPower.java
+++ calcPower.java
@@ -1,8 +1,36 @@
 public class calcPower {
     public static int  calcPower(int x,int n){
+//        high stack height
+//
+//    if (x==0){
+//        return 0;
+//    }
+//    if (n==0){
+//        return 1;
+//    }
+//    int power1=calcPower(x,n-1);
+//    int power=x*power1;
+//    return power;
+
+//        logn stack height
+        if (x==0){
+            return 0;
+        }
+        if (n==0){
+            return 1;
+        }
+        if (n%2==0){
+            return calcPower(x,n/2)*calcPower(x,n/2);
+        }
+        else{
+            return calcPower(x,n/2)*calcPower(x,n/2)*x;
+        }
+
+
 
     }
     public static void main(String[]args){
+        System.out.println(calcPower(4,3));
 
     }
 }
