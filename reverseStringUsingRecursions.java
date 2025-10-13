1da191108082dad97bda64f4f94e985942e2074d
Ruhaan Narang <techieruhaan@gmail.com>
2025-10-13 20:52:46 +0530

    recursion reverse a string question

--- reverseStringUsingRecursions.java
+++ reverseStringUsingRecursions.java
@@ -1,9 +1,15 @@
 public class reverseStringUsingRecursions {
-    public static void reverse(String s){
-
+    public static void reverse(String s,int i){
+        if(i==s.length()){
+            return;
+        }
+        reverse(s,i+1);
+        System.out.print(s.charAt(i));
 
     }
     public static void main(String []args){
-
+        String s="Ruhaan";
+        int i=0;
+        reverse(s,i);
     }
 }
