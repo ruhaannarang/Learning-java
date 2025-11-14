d2644867e5796a6fef228c55968ba3c6af24ccee
Ruhaan Narang <techieruhaan@gmail.com>
2025-11-14 20:32:20 +0530

    multiplication of matrix question

--- matrixMultiplication.java
+++ matrixMultiplication.java
@@ -1,31 +1,45 @@
+import java.util.*;
 public class matrixMultiplication {
     public static void main(String[]args){
-        int[][] first = {
-                {1, 2, 3},
-                {4, 5, 6},
-                {7, 8, 9}
-        };
-        int[][] second = {
-                {1, 2, 3},
-                {4, 5, 6},
-                {7, 8, 9}
-        };
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter no of rows of first matrix");
+        int RowsMatrixOne=sc.nextInt();
+        System.out.println("Enter no of columns of first matrix");
+        int columnMatrixOne=sc.nextInt();
+        int[][] first = new int[RowsMatrixOne][columnMatrixOne];
+        for (int i = 0; i <RowsMatrixOne; i++) {
+            for (int j = 0; j < columnMatrixOne; j++) {
+                System.out.println("Enter element of"+i+j);
+                first[i][j]=sc.nextInt();
+            }
+        }
+        System.out.println("Enter no of rows of second matrix");
+        int RowsMatrixTwo=sc.nextInt();
+        System.out.println("Enter no of columns of second matrix");
+        int columnMatrixTwo=sc.nextInt();
+        int[][] second = new int[RowsMatrixTwo][columnMatrixTwo];
+        for (int i = 0; i <RowsMatrixTwo; i++) {
+            for (int j = 0; j < columnMatrixTwo; j++) {
+                System.out.println("Enter element of"+i+j);
+                second[i][j]=sc.nextInt();
+            }
+        }
         int i=0;
         int j=0;
         int k=0;
-        int m=first.length;
-        int n=first[0].length;
-        int a=second[0].length;
+        int m=RowsMatrixOne;
+        int n=columnMatrixOne;
+        int a=columnMatrixTwo;
         int ans[][]=new int[m][a];
         for (i = 0; i < m; i++) {
             for ( j = 0; j < a; j++) {
                     ans[i][j]=0;
-                for ( k = 0; k < a ; k++) {
+                for ( k = 0; k < n ; k++) {
                     ans[i][j]=ans[i][j]+(first[i][k]*second[k][j]);
                 }
             }
         }
-        for ( i = 0; i < n; i++) {
+        for ( i = 0; i < m; i++) {
             for ( j = 0; j < a; j++) {
                 System.out.print(ans[i][j] + " ");
             }
