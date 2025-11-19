542d63f5a50e7d259174bbc62d1fdd7cc3987bc5
Ruhaan Narang <techieruhaan@gmail.com>
2025-11-19 19:40:47 +0530

    insertion sort

--- insertionSort.java
+++ insertionSort.java
@@ -1,17 +1,21 @@
 public class insertionSort {
     public static void main(String args[]){
         int arr[]={7,8,3,1,2};
-        for(int i=1;i<arr.length;i++){
-            int current=arr[i];
-            int j=i-1;
-            while(j>=0 && current<arr[j]){
-                arr[j+1]=arr[j];
-                j--;
+        for(int i=1;i<arr.length-1;i++){
+            for (int j = i+1; j >0 ; j--) {
+                if (arr[j]<arr[j-1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j-1];
+                    arr[j-1]=temp;
+                }
+                else{
+                    break;
+                }
             }
-
+            
         }
         for (int i=0;i<arr.length;i++){
-            System.out.println(arr[i]);
+            System.out.print(arr[i]+" ");
         }
     }
 }
