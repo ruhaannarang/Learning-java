64a6a7113165c6e97a772964ec965c3b5b5acbef
Ruhaan Narang <techieruhaan@gmail.com>
2025-11-19 19:03:37 +0530

    selection sort

--- selectionSort.java
+++ selectionSort.java
@@ -1,23 +1,26 @@
 public class selectionSort {
     public static void main(String args[]){
         int arr[]={7,8,3,1,2};
-        for (int j = arr.length-1; j >=0 ; j++) {
+        for (int j = arr.length-1; j >=0 ; j--) {
             int highest=arr[0];
-            for (int i = 0; i < arr.length-(arr.length-j); i++) {
+            int highestIndex=0;
+
+            for (int i = 0; i <= j; i++) {
                 if(highest<arr[i]){
                     highest=arr[i];
+                    highestIndex=i;
                 }
-                int temp=arr[j];
-                arr[j]=highest;
-                arr[i]=temp;
-            }
 
+            }
+            int temp=arr[j];
+            arr[j]=highest;
+            arr[highestIndex]=temp;
         }
 
 //        System.out.println(highest);
 
         for(int i=0;i<arr.length;i++){
-            System.out.println(arr[i]);
+            System.out.print(arr[i]);
         }
     }
 }
