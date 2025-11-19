a69eb29cd72dda86913e1225fff71b759c9577a7
Ruhaan Narang <techieruhaan@gmail.com>
2025-11-19 18:54:28 +0530

    bubble sort

--- bubbleSort.java
+++ bubbleSort.java
@@ -1,17 +1,18 @@
 public class bubbleSort {
     public static void main(String[]args){
         int num[] ={7,8,3,1,2};
-        for(int i=0;i<num.length-1;i++){
-            for(int j=0;j<num.length-1-i;j++){
-                if(num[j]>num[j+1]){
-                    int temp=num[j];
+        for (int i = 0; i < num.length-1; i++) {
+            for (int j = 0; j < num.length-i-1; j++) {
+                if (num[j]>num[j+1]){
+                    int temp = num[j];
                     num[j]=num[j+1];
                     num[j+1]=temp;
                 }
             }
+
         }
         for (int i=0;i<num.length;i++){
-            System.out.println(num[i]);
+            System.out.print(num[i]+ " ");
         }
     }
 }
--- selectionSort.java
+++ selectionSort.java
@@ -1,17 +1,21 @@
 public class selectionSort {
     public static void main(String args[]){
         int arr[]={7,8,3,1,2};
-        for (int i=0;i<arr.length;i++){
-            int smallest=i;
-            for(int j=i+1;j<arr.length;j++){
-                if(arr[smallest]>arr[j]){
-                    smallest=j;
+        for (int j = arr.length-1; j >=0 ; j++) {
+            int highest=arr[0];
+            for (int i = 0; i < arr.length-(arr.length-j); i++) {
+                if(highest<arr[i]){
+                    highest=arr[i];
                 }
+                int temp=arr[j];
+                arr[j]=highest;
+                arr[i]=temp;
             }
-            int temp=arr[smallest];
-            arr[smallest]=arr[i];
-            arr[i]=temp;
+
         }
+
+//        System.out.println(highest);
+
         for(int i=0;i<arr.length;i++){
             System.out.println(arr[i]);
         }
