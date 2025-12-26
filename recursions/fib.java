937b3fd9756bc458a1eecae012ef98f9fe62ea93
Ruhaan Narang <techieruhaan@gmail.com>
2025-12-27 02:18:27 +0530

    recursions fib series

---
+++ recursions/fib.java
@@ -0,0 +1,14 @@
+package recursions;
+
+public class fib {
+    static int  fib(int n){
+        if(n<2){
+            return n;
+        }
+        return fib(n-1)+fib(n-2);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(fib(7));
+    }
+}
