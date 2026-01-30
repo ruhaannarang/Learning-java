370cd1474fb78c54d9c0817c94077f0c4f6f67db
Ruhaan Narang <techieruhaan@gmail.com>
2026-01-30 21:47:25 +0530

    odd even using bits

---
+++ BitManipulation2/oddeven.java
@@ -0,0 +1,14 @@
+package BitManipulation2;
+
+public class oddeven {
+    public static void main(String[] args) {
+        int n=7;
+        if((n & 1) == 1){
+            System.out.println("odd");
+        }
+        else{
+            System.out.println("even");
+        }
+    }
+
+}
