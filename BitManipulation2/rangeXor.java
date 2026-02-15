9eb2246ed364178b5dca45e72434938ea951f29f
Ruhaan Narang <techieruhaan@gmail.com>
2026-02-15 21:23:05 +0530

    java bits  ..

---
+++ BitManipulation2/rangeXor.java
@@ -0,0 +1,20 @@
+package BitManipulation2;
+
+public class rangeXor {
+    public static void main(String[] args) {
+        int a=3;
+        int b=9;
+        System.out.println(XorA(b)^XorA(a-1));
+    }
+    static int XorA(int a){
+        if(a%4==0){
+            return a;
+        }if (a%4==1) {
+            return 1;
+        }
+        if (a%4==2) {
+            return a+1;
+        }
+        return 0;
+    }
+}
