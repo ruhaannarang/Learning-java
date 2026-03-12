2bda680bdec652dfc0e93cccb231825c71170e22
Ruhaan Narang <techieruhaan@gmail.com>
2026-03-13 00:47:11 +0530

    arraylist revisionn

--- ArrayListExample.java
+++ ArrayListExample.java
@@ -4,28 +4,26 @@ public class ArrayListExample {
     public static void main(String []args){
         Scanner sc = new Scanner(System.in);
         ArrayList<Integer> list = new ArrayList<>(10);
-        list.add(67);
-        list.add(34);
-        list.add(663);
-        list.add(32);
-        list.add(6);
-        list.add(644);
-        list.add(6);
         list.add(7);
-        list.add(88);
-        list.add(0);
-        list.add(6478);
+        list.add(67);
+        list.add(1,4);
         list.add(6874);
-        System.out.println(list.contains(7));
-        list.set(0,888);
-        list.remove(7);
+        list.add(56);
+        list.add(77);
         System.out.println(list);
-        for (int i = 0; i < 5; i++) {
-            list.add(sc.nextInt());
-        }
+        list.remove(2);
+//        System.out.println(list.contains(7));
+//        list.set(0,888);
+
         System.out.println(list);
+//        for (int i = 0; i < 5; i++) {
+//            list.add(sc.nextInt());
+//        }
+//        System.out.println(list);
         for (int i = 0; i < 5; i++) {
             System.out.println(list.get(i));
         }
+        Collections.sort(list);
+        System.out.println(list);
     }
 }
