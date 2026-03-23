5245ad167159e413d04637e01ad5bd8362b42e3c
Ruhaan Narang <techieruhaan@gmail.com>
2026-03-23 22:33:05 +0530

    folder

--- even.java
+++
@@ -1,8 +0,0 @@
-public class even {
-    public static void main(String []args){
-        int a=90;
-        for(int i=1;(i*2)<a;i++){
-            System.out.println(i*2);
-        }
-    }
-}
--- hello.java
+++
@@ -1,8 +0,0 @@
-class hello{
-    public static void main(String[] args) {
-        //output
-        System.out.println("hello world with Ruhaan");
-        System.out.println("hello world with Ruhaan");
-        System.out.print("hello world with Ruhaan");
-    }
-}
