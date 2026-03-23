f53fba25c068f06e306f7ef2afa631cdf43feac5
Ruhaan Narang <techieruhaan@gmail.com>
2026-03-23 22:50:42 +0530

    character hashing

--- Hashing/characterHashing.java
+++ Hashing/characterHashing.java
@@ -1,4 +1,14 @@
 package Hashing;
 
 public class characterHashing {
+    public static void main(String[] args) {
+        String str="asdhasfghjasd";
+        char target='r';
+        int[] hash=new int[26];
+        for(int i=0;i<str.length();i++){
+            hash[str.charAt(i)-'a']++;
+        }
+        System.out.println(hash[target-'a']);
+
+    }
 }
